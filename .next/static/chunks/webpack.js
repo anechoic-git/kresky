57edc8291a3895f18834641cfa379c052f7cca6d
Jackson <Jackson.harris3@gmail.com>
2020-09-02 19:35:52 -0700

    remove node modules & init remaining base pages

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cb3a17d0f27b3098e4f0";
+/******/ 	var hotCurrentHash = "d300117c344649bd2019";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
